e8346c3bdaaf853fb9191e507dab75c25e456383
Varun Bajlotra <varunbajlotra@gmail.com>
2019-11-17 17:48:43 +0530

    YO

--- Test.js
+++ Test.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-const PORT = process.env.PORT || 4321
+// const PORT = process.env.PORT || 4321
 
-app.get('/',(req,res)=>{
-    res.send('Hello! This is a sample app')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Hello! This is a sample app')
+// })
 
-app.get('/app',(req,res)=>{
-    res.send('Hello you are in our main app')
-})
+// app.get('/app',(req,res)=>{
+//     res.send('Hello you are in our main app')
+// })
 
-app.listen(PORT,()=>{
-    console.log('Server started at http://localhost:4321 ' + PORT)
-})
+// app.listen(PORT,()=>{
+//     console.log('Server started at http://localhost:4321 ' + PORT)
+// })
